7a19f4e22fab2b385b8ccc23d30a709e2ee692ff
Nethelbert <gilblasse@gmail.com>
2019-12-12 14:38:49 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,19 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+  
+  def self.column_names
+    sql = <<-SQL
+    PRAGMA table_info( #{table_name} )
+    SQL
+    table_schema = DB[:conn].execute(sql)
+    headers = table_schema.map{|hash| hash["name"] }
+  end
+  
+  
 end
+
+
--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,10 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
+  self.column_names.each {|attr| attr_accessor attr.to_sym}
+  
+  def initialize(options = {})
+    options.each {|k,v| self.send("#{k}",v) }
+  end
+  
 end
