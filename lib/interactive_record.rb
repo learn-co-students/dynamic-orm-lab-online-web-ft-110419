addaae3eae1d91c8cbc55f8d8ff65ce14c7028f0
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-12-18 20:51:49 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,9 +3,11 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  
   def self.table_name
     self.to_s.downcase.pluralize
   end
+  
   def self.column_names
     DB[:conn].results_as_hash = true
     
@@ -18,35 +20,47 @@ class InteractiveRecord
       end
       column_names.compact
   end
+  
   def initialize(options={})
     options.each do |k, v|
       self.send("#{k}=", v)
     end
   end
   
-  self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
-  end
-  
   def some_instance_method
     self.class.some_class_method
   end
+  
   def table_name_for_insert
     self.class.table_name
   end
+  
   def col_names_for_insert
-    self.class.column_names.delet_if {|col| col == "id"}.join(", ")
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
+  
   def values_for_insert
     values = []
-    self.column_names.each do |col_name|
+    self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
     values.join(", ")
   end
+  
   def save
-    DB[:conn].execute("INSERT INTO #{table_name_for_insert}
-    (#{col_names_for_insert}) VALUES (?)", [values_for_insert])
-    @id = DB[:conn].execute("SELECT last_insrted_rowif() FROM #{table_name_for_insert}")[0][0]
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+  
+  def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", [name])
+  end
+  
+  def self.find_by(hash)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys[0].to_s} = '#{hash.values[0].to_s}'"
+    DB[:conn].execute(sql)
+  
   end
 end
