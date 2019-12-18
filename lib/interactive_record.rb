5ee6fb1ef4c4465acf8cde92d80df177e6fddc4d
Christian Jimenez <christianjimenez0123@gmail.com>
2019-12-18 22:23:15 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -17,11 +17,39 @@ class InteractiveRecord
    
   end
   def self.column_names
-    binding.pry
+    
     array = []
     sql = "pragma table_info('#{table_name}')"
-    DB[:conn].execute(sql)
+    table_info = DB[:conn].execute(sql)
+    table_info.each do |a|
+      array.push(a["name"])
+    end
+    array
   end
-  def initialize
+  def table_name_for_insert
+    Student.table_name
   end
+  def col_names_for_insert
+   Student.column_names.last(2).join(", ")
+  end
+  def values_for_insert
+    values = []
+    Student.column_names.each do |col|
+      values << "'#{self.send(col)}'" if self.send(col)
+    end
+    values.join(", ")
+  end
+ def save
+  sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+  DB[:conn].execute(sql)
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+ end
+ def self.find_by_name(name)
+   sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+   DB[:conn].execute(sql)
+ end
+ def self.find_by(attribute)
+   sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys[0].to_s} = '#{attribute.values[0]}'"
+   DB[:conn].execute(sql)
+ end
 end
--- lib/student.rb
+++ lib/student.rb
@@ -3,9 +3,16 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 require "pry"
 class Student < InteractiveRecord
-  attr_accessor :name, :id, :grade
-
-  
+  # attr_accessor :name, :id, :grade
+  self.column_names.each do |a|
+  attr_accessor  a.to_sym
+  end
+   def initialize(hash={})
+    hash.each do |a,b|
+      self.send("#{a.to_s}=",b)
+   
+    end
+  end
     
 
 end
